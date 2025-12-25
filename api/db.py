59d1ac6a6a7c8f6f93cebaaf7d01c4efa3aa5903
maitech2008-prog <m.aitech2008@gmail.com>
2025-12-25 08:47:15 +0300

    Create db.py

---
+++ api/db.py
@@ -0,0 +1,15 @@
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Example test function
+def test_connection():
+    res = supabase.table("technicians").select("*").limit(1).execute()
+    return res.data
